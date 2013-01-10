3401b61d6a59d3b63c9edc95ff60630ab6ce8d70
Unai Aguilera <gkalgan@gmail.com>
2013-01-10 19:19:34 +0100

    Implementation of Runge-Kutta for two equations with (x, y, t) variables

--- eq_diferenciales.py
+++ eq_diferenciales.py
@@ -14,4 +14,31 @@ def runge_kutta(h, n, f, x_0, y_0, verbose=False):
 		y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 		x += h
 
-	return y
+	return x, y
+
+
+def calcular_estimaciones(h, f, x, y, t, verbose=False):
+	k1 = h * f(x, y, t)
+	k2 = h * f(x + h / 2, y + k1 / 2, t)
+	k3 = h * f(x + h / 2, y + k2 / 2, t)
+	k4 = h * f(x + h, y + k3, t)
+
+	if verbose: print "%.5f \t %.5f \t %.5f \t %.5f \t %.5f \t %.5f \t %.5f" % (t, x, y, k1, k2, k3, k4)
+
+	return k1, k2, k3, k4
+
+def runge_kutta_sistema(h, n, f, g, x_0, y_0, t_0, verbose=False):
+	t = t_0
+	x = x_0
+	y = y_0
+
+	for i in range(n + 1):
+		kx1, kx2, kx3, kx4 = calcular_estimaciones(h, f, x, y, t, verbose)
+		ky1, ky2, ky3, ky4 = calcular_estimaciones(h, g, x, y, t, verbose)
+
+		x = x + (kx1 + 2 * kx2 + 2 * kx3 + kx4) / 6
+		y = y + (ky1 + 2 * ky2 + 2 * ky3 + ky4) / 6
+
+		t += h
+
+	return t, x, y
--- test_runge_kutta.py
+++ test_runge_kutta.py
@@ -1,7 +1,7 @@
 import eq_diferenciales
 
-def df_dx(x, y):
+def dy_dx(x, y):
 	return -2 * x -y
 
 print 'Metodo Runge-Kutta'
-eq_diferenciales.runge_kutta(0.1, 5, df_dx, 0.0, -1.0, verbose=True)
+eq_diferenciales.runge_kutta(0.1, 5, dy_dx, 0.0, -1.0, verbose=True)
