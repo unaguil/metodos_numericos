8af8d44b9a95166e336276b9702b9ba5a9a921c3
Unai Aguilera <gkalgan@gmail.com>
2013-01-10 18:26:58 +0100

    Method statements reordered

--- eq_diferenciales.py
+++ eq_diferenciales.py
@@ -8,9 +8,10 @@ def runge_kutta(h, n, f, x_0, y_0, verbose=False):
 		k2 = h * f(x + h / 2, y + k1 / 2)
 		k3 = h * f(x + h / 2, y + k2 / 2)
 		k4 = h * f(x + h, y + k3)
-		new_y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 		if verbose: print "%.1f \t %.5f \t %.4f \t %.4f \t %.4f \t %.4f" % (x, y, k1, k2, k3, k4)
-		
+
+		y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 		x += h
-		y = new_y
+
+	return y
