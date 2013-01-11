4a5ccb881ec7d9a392e6de3a8bdf997b37c16ee8
Unai Aguilera <gkalgan@gmail.com>
2013-01-11 20:42:33 +0100

    New case added

---
+++ caso_anarmonico_forzado_amortiguado_s1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*- 
+
+from metodos import eq_diferenciales
+from metodos import transformada_fourier
+import math
+import graficas
+
+def f(x, y, t):
+	return y
+
+def g(x, y, t):
+	return 0.516 * math.cos(1.2 * t) - x - 0.52 * y - x * x
+
+h = 0.01
+num_periodos = 5
+x_0 = 0.0
+y_0 = 0.0
+t_0 = 0.0
+graficas.mostrar_graficas(h, num_periodos, f, g, x_0, y_0, t_0, 'oscilador no lineal forzado y amortiguado - S1')
