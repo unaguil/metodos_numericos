43e8cfc3848b3be95431dba7ab3c1d8ad6254485
Unai Aguilera <gkalgan@gmail.com>
2013-01-11 20:27:46 +0100

    Basic cases added

---
+++ caso_forzado_amortiguado.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*- 
+
+from metodos import eq_diferenciales
+from metodos import transformada_fourier
+import math
+import graficas
+
+def f(x, y, t):
+	return y
+
+def g(x, y, t):
+	return math.cos(t) - x - 1.5 * y
+
+h = 0.01
+num_periodos = 5
+x_0 = 0.0
+y_0 = 0.0
+t_0 = 0.0
+graficas.mostrar_graficas(h, num_periodos, f, g, x_0, y_0, t_0, 'oscilador lineal forzado y amortiguado')
