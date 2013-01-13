7200b6f6e56dc73027f37286dcac4234d0eb740b
Unai Aguilera <gkalgan@gmail.com>
2013-01-13 19:28:43 +0100

    Comments added

--- metodos/eq_diferenciales.py
+++ metodos/eq_diferenciales.py
@@ -18,14 +18,14 @@ def runge_kutta(h, n, f, x_0, y_0, verbose=False):
 	return x, y
 
 def runge_kutta_sistema(h, n, f, g, x_0, y_0, t_0, verbose=False):
-	t = t_0
+	t = t_0 # condiciones iniciales del sistema
 	x = x_0
 	y = y_0
 
 	tabla = [(t, x, y)]
 
-	for i in range(n + 1):
-		k1 = h * f(x, y, t)
+	for i in range(n + 1): #bucle hasta el número de pasos indicado
+		k1 = h * f(x, y, t) #cálculo de los coeficientes
 		l1 = h * g(x, y, t)
 
 		k2 = h * f(x + k1 / 2, y + l1 / 2, t + h / 2)
@@ -37,10 +37,13 @@ def runge_kutta_sistema(h, n, f, g, x_0, y_0, t_0, verbose=False):
 		k4 = h * f(x + k3, y + l3, t + h)
 		l4 = h * g(x + k3, y + l3, t + h)
 
+		#incremento de las variables dependientes (x, y)
+		# y de la independiente (t)
 		x = x + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 		y = y + (l1 + 2 * l2 + 2 * l3 + l4) / 6
 		t += h
 
+		#los resultados son almacenados en una lista de elementos (t, x, y)
 		tabla.append((t, x, y))
 
 	return tabla
