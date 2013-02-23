096f0c68ad17c2926841018a89a14cdc3fa80a47
Unai Aguilera <gkalgan@gmail.com>
2013-02-23 18:24:09 +0100

    Modified euler example changed.

--- metodos/derivacion_euler.py
+++ metodos/derivacion_euler.py
@@ -1,3 +1,6 @@
+import math
+from matplotlib import pyplot
+from matplotlib.ticker import MultipleLocator
 
 def euler(h, n, f, x_0, y_0):
 	y = y_0
@@ -13,23 +16,43 @@ def euler(h, n, f, x_0, y_0):
 def euler_mod(h, n, f, x_0, y_0):
 	y = y_0
 	x = x_0
+
+	x_values = [x]
+	y_values = [y]
+
 	for i in range(n + 1):
 		f_val = f(x, y)
 		hf = h * f_val
 		y_p = y + hf
 		x_p = x + h
 		f_p = f(x_p, y_p)
-		hf_p = h * f_p 
-		y_a = y + h/2 * (f_val + f_p)
-		print '%.1f \t %.5f \t %.4f \t %.4f \t %.4f \t %.4f' % (x, y, hf, y_p, hf_p, y_a)
+		f_m = 1/2.0 * (f_val + f_p)
+		hfm = h * f_m
+		y_a = y + hfm
+		print '%.1f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f \t %.4f' % (x, y, f_val, hf, y_p, f_p, f_m, hfm, y_a)
 		x = x_p
-		y = y_a		 
+		y = y_a
+
+		x_values.append(x)
+		y_values.append(y)
+
+	return x_values, y_values
 
 def df_dx(x, y):
-	return -2*x + - y
+	return y * math.cos(x + y)
 
-print 'Metodo de Euler'
-euler(0.1, 4, df_dx, 0.0, -1.0)
+#print 'Metodo de Euler'
+#euler(0.1, 4, df_dx, 0.0, -1.0)
 
 print 'Metodo de Euler Modificado'
-euler_mod(0.1, 4, df_dx, 0.0, -1.0)
+x_values, y_values = euler_mod(0.1, 200, df_dx, 0.0, 1.0)
+
+minorLocator = MultipleLocator(1)
+
+pyplot.plot(x_values, y_values)
+pyplot.xlabel('t')
+pyplot.ylabel('y(t)')
+pyplot.axes().xaxis.set_minor_locator(minorLocator)
+pyplot.grid(True)
+
+pyplot.show()
